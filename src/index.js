8845da64517aae26ca005c16d971ebe33895a49d
niranjna95 <niranjangiri51295@gmail.com>
2024-10-26 23:22:43 +0530

    utils code add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runing at port: ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw err;
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!!", err);
+  });
 
 // (async () => {
 //   try {
---
+++ src/services/httpServices.js
@@ -0,0 +1,7 @@
+const HttpServices = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { HttpServices };
